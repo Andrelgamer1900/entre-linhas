3c1b0fc38fac53963ba9cfe3d3fd846dec700d16
Andrelgamer1900 <124527576+Andrelgamer1900@users.noreply.github.com>
2026-05-02 12:11:24 -0400

    Update: Abrindo direto na Biblioteca para facilitar apresentação

--- App.js
+++ App.js
@@ -1,40 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from './src/config/firebase';
 
 // Telas
-import LoginScreen from './src/screens/LoginScreen';
 import HomeScreen from './src/screens/HomeScreen';
+import LoginScreen from './src/screens/LoginScreen';
 
 const Stack = createStackNavigator();
 
 export default function App() {
-  const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      setUser(user);
-      setLoading(false);
-    });
-    return unsubscribe;
-  }, []);
-
-  if (loading) return null; // Poderia ser uma tela de Splash
-
+  // André, alterei aqui para abrir direto na Home (Biblioteca)
+  // Assim você consegue mostrar o app funcionando sem depender do Firebase da sua colega.
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {user ? (
-          <Stack.Screen name="Home" component={HomeScreen} />
-        ) : (
-          <>
-            <Stack.Screen name="Login" component={LoginScreen} />
-            {/* Aqui você pode adicionar a tela de Registro depois */}
-          </>
-        )}
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
