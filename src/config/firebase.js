46c63d4dc7804b3d1e9766c4d1a4d3b491565732
André leandro <124527576+Andrelgamer1900@users.noreply.github.com>
2026-05-14 20:12:26 -0300

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,8 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// André, você precisará substituir esses valores pelos dados do seu projeto no console do Firebase
-// Se a sua colega já criou o projeto, peça para ela esses dados ou pegue no console do Firebase.
+
 const firebaseConfig = {
   apiKey: "SUA_API_KEY",
   authDomain: "SEU_AUTH_DOMAIN",
